c08788c074c8979d705c2541036c86c55806321f
baucuenca <jnb.cuenca1507@gmail.com>
2025-08-23 16:54:05 -0300

    Creada pagina de adminitracion de miembros

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Loans from "./pages/Loans";
+import Members from "./pages/Members";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/loans" element={<Loans />} />
+        <Route path="/members" element={<Members />} />
       </Routes>
     </>
   );
---
+++ src/pages/Members.tsx
@@ -0,0 +1,26 @@
+// Pagina de administración de miembros
+
+import MenuCard from "../components/MenuCard";
+import GoBackButton from "../components/GoBackButton";
+
+function Members() {
+  return (
+    <main className="min-h-screen bg-slate-50 flex items-center justify-center p-6">
+      <div className="w-full max-w-sm">
+        <h1 className="text-4xl font-bold text-center mb-4">Miembros</h1>
+        <h2 className="text-lg  mb-4 text-gray-400">
+          Opciones de administración:
+        </h2>
+
+        {/* Menu */}
+        <div className="space-y-4">
+          <MenuCard to="/members/new" title="Nuevo Miembro" />
+          <MenuCard to="/members/manage" title="Gestionar Miembros" />
+          <GoBackButton />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Members;
