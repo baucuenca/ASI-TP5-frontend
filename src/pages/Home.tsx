115282e246ebc97eeec31fb28915d0c5a734c653
baucuenca <jnb.cuenca1507@gmail.com>
2025-08-23 16:34:16 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,9 @@ function Home() {
     <main className="min-h-screen bg-slate-50 flex items-center justify-center p-6">
       <div className="w-full max-w-sm">
         <h1 className="text-4xl font-bold text-center mb-4">Biblioteca</h1>
-        <h2 className="text-lg  mb-4 text-gray-400">Opciones de gestión:</h2>
+        <h2 className="text-lg  mb-4 text-gray-400">
+          Opciones de administración:
+        </h2>
 
         {/* Menu */}
         <div className="space-y-4">
