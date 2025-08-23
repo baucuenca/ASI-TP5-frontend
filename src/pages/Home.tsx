b31f13c2d3687b7b482d0b9b6011c7b49e5f6881
baucuenca <jnb.cuenca1507@gmail.com>
2025-08-23 16:33:50 -0300

    Implementado el menu en Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,22 @@
 // Pagina de inicio
 
+import MenuCard from "../components/MenuCard";
+
 function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <main className="min-h-screen bg-slate-50 flex items-center justify-center p-6">
+      <div className="w-full max-w-sm">
+        <h1 className="text-4xl font-bold text-center mb-4">Biblioteca</h1>
+        <h2 className="text-lg  mb-4 text-gray-400">Opciones de gestión:</h2>
+
+        {/* Menu */}
+        <div className="space-y-4">
+          <MenuCard to="/loans" title="Préstamos" />
+          <MenuCard to="/members" title="Miembros" />
+          <MenuCard to="/books" title="Libros" />
+        </div>
+      </div>
+    </main>
   );
 }
 
