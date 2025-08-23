d59159059c83562e856f20809bd17cadd3e09edd
baucuenca <jnb.cuenca1507@gmail.com>
2025-08-23 17:26:45 -0300

    Separadas rutas asociadas a los prestamos

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import "./index.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import Loans from "./pages/Loans";
 import booksRoutes from "./routes/booksRoutes";
 import membersRoutes from "./routes/membersRoutes";
+import loansRoutes from "./routes/loansRoutes";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/loans" element={<Loans />} />
+        {loansRoutes}
         {membersRoutes}
         {booksRoutes}
       </Routes>
