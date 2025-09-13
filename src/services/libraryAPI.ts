369f1b9ac099b46fd8e944bcdd76e612e5fadab3
baucuenca <jnb.cuenca1507@gmail.com>
2025-09-13 17:22:54 -0300

    Agregadas direcciones del servidor

--- src/services/libraryAPI.ts
+++ src/services/libraryAPI.ts
@@ -3,7 +3,10 @@
 import axios from "axios";
 import type { AxiosRequestConfig } from "axios"; // Se debe importar como un "tipo" de datos
 
-const BASE_URL = "http://127.0.0.1:8000";
+const SERVER_PUBLIC_IP = import.meta.env.VITE_SERVER_PUBLIC_IP;
+const BACKEND_PORT = import.meta.env.VITE_BACKEND_PORT;
+
+const BASE_URL = `http://${SERVER_PUBLIC_IP}:${BACKEND_PORT}`;
 
 type HttpMethod = "get" | "post" | "patch" | "delete";
 
